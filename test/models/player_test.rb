9b0aaf57f3a7c0a090b2e65ee00f43569701cfc1
othman853 <anuar.yasser@gmail.com>
2016-03-22 16:53:09 -0300

    Testing Player model with fixtures

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -3,43 +3,41 @@ require 'test_helper'
 class PlayerTest < ActiveSupport::TestCase
 
   test "Player should be invalid when they have no email" do
-    invalid_email = "invalid"
-    player = Player.new(name: "Player 1", email: invalid_email)
+    player = players(:first_player)
+    player.email = "invalid"
 
     assert ! player.valid?, "Email's format should have been checked"
   end
 
   test "Player should be invalid when they have an email out of the correct format" do
 
-    player = Player.new
+    player = players(:first_player)
+    player.email = nil
 
     assert ! player.valid?, "Email's existence should have been checked"
   end
 
   test "Player should be invalid when they have no name" do
-    player = Player.new
+    player = players(:first_player)
+    player.name = nil
 
     assert ! player.valid?, "Name's existence should have been checked"
   end
 
   test "When there is no nick name, presentation_name should return the actual name of the player" do
-    expected = "Player1"
-    player = Player.new(name: expected, nickname: nil)
+    player = players(:player_with_no_nickname)
 
-    assert_equal expected, player.presentation_name, "#{expected} was expected but the result was #{player.presentation_name}"
+    assert_equal player.name, player.presentation_name, "#{player.name} was expected but the result was #{player.presentation_name}"
   end
 
   test "When there is a nick name, presentation_name should return it" do
-    expected = "nick_name"
-    player = Player.new(name: "Player", nickname: expected)
+    player = players(:player_with_nickname)
 
-    assert_equal expected, player.presentation_name, "#{expected} was expected but the result was #{player.presentation_name}"
+    assert_equal player.nickname, player.presentation_name, "#{player.nickname} was expected but the result was #{player.presentation_name}"
   end
 
   test "Player should be valid when they have a Name and an Email that matches the correct format" do
-    correct_name = "Aname"
-    correct_email = "email@company.com"
-    player = Player.new(name: correct_name, email: correct_email)
+    player = players(:first_player)
 
     assert player.valid?, "Player is still invalid when they have a correct name and email"
   end
