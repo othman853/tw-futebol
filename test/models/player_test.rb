d23205a0e429eb462e252e22cfcc792f32e484c2
othman853 <anuar.yasser@gmail.com>
2016-03-22 20:07:06 -0300

    Emails should be unique

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -6,5 +6,6 @@ class PlayerTest < ActiveSupport::TestCase
   should validate_presence_of (:email)
   should_not allow_value('invalid_email').for(:email)
   should allow_value('player@twfutebol.com').for(:email)
+  should validate_uniqueness_of (:email)
 
 end
