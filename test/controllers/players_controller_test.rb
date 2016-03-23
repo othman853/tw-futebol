9b938bd10b7ea5f914a2b14453f87a0cdcc7679d
othman853 <anuar.yasser@gmail.com>
2016-03-23 20:28:26 -0300

    [REFACTOR] Adding a weird test to controller. Must find the proper way to do this.

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PlayersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get new player page" do
+    get :new
+    assert_response :success
+  end
+
+  test "Should create a user and ignore not whitelisted param" do
+
+    post :create, player: { name: 'a', email: 'a@a.com', ignored: "ignore" }
+
+    assert_not_nil assigns(:player)
+    assert_equal 'a', assigns(:player).name
+    assert_equal 'a@a.com', assigns(:player).email
+  end
+
 end
